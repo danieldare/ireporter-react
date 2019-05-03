2b0888189e65ed154b4afd071fcf51c3074e1982
Segun Oluwadare <dodanieloluwadare@gmail.com>
2019-05-03 08:27:26 +0100

    feat(profile) Profile functionality

---
+++ src/reducers/profileReducer.js
@@ -0,0 +1,52 @@
+import {
+  GET_INCIDENTS_ERROR,
+  INCIDENTS_LOADING,
+  GET_INCIDENTS_SUCCESS,
+  DELETE_INCIDENT,
+  INCIDENT_DELETING
+} from '../actions/actionTypes/types';
+
+const initialState = {
+  incidents: [],
+  isLoading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_INCIDENTS_SUCCESS:
+      return {
+        ...state,
+        incidents: action.payload.data,
+        isLoading: false,
+        error: null
+      };
+    case INCIDENTS_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case GET_INCIDENTS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case DELETE_INCIDENT:
+      return {
+        ...state,
+        isDeleting: false,
+        incidents: state.incidents.filter(
+          incident => incident.id !== action.payload
+        )
+      };
+    case INCIDENT_DELETING:
+      return {
+        ...state,
+        isDeleting: true
+      };
+    default:
+      return state;
+  }
+};
